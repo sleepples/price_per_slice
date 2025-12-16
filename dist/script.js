f34e99e79792132240354b99a8da224635d0e43d
Spencer Rotella <3019849@rps.edu>
2025-12-16 10:11:35 -0500

    changed styleing

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,10 @@
 "use strict";
-var price_input = document.querySelector("#price_input input");
-var diameter_input = document.querySelector("#diameter_input input");
+var price_input = document.querySelector("#price_input");
+var diameter_input = document.querySelector("#diameter_input");
+var calc_result = document.querySelector("#calc_result");
 var pi = 3.14159265359;
 function calculate() {
-    console.log(2 * pi * Math.pow((parseFloat(diameter_input.value) / 2), 2));
+    var area = parseFloat((pi * Math.pow((parseFloat(diameter_input.value) / 2), 2)).toFixed(2));
+    var price_per_square_inch = parseFloat((parseFloat(price_input.value) / area).toFixed(2));
+    calc_result.value = "price per isq: ".concat(price_per_square_inch, " | area: ").concat(area);
 }
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,11 @@
-const price_input: HTMLInputElement = document.querySelector("#price_input input");
-const diameter_input: HTMLInputElement = document.querySelector("#diameter_input input");
+const price_input: HTMLInputElement = document.querySelector("#price_input");
+const diameter_input: HTMLInputElement = document.querySelector("#diameter_input");
+const calc_result: HTMLInputElement = document.querySelector("#calc_result");
 const pi: number = 3.14159265359;
 
 function calculate(): void {
-  console.log(2*pi*(parseFloat(diameter_input.value)/2)**2);
+  var area = parseFloat((pi*(parseFloat(diameter_input.value)/2)**2).toFixed(2));
+  var price_per_square_inch = parseFloat((parseFloat(price_input.value)/area).toFixed(2));
+
+  calc_result.value = `price per isq: ${price_per_square_inch} | area: ${area}`;
 }
