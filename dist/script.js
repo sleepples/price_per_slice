498952868a1a52608864224d06d99f4b229d3510
Spencer Rotella <3019849@rps.edu>
2026-01-06 09:11:30 -0500

    added checks to make sure the input is valid

--- dist/script.js
+++ dist/script.js
@@ -6,5 +6,16 @@ var pi = 3.14159265359;
 function calculate() {
     var area = parseFloat((pi * Math.pow((parseFloat(diameter_input.value) / 2), 2)).toFixed(2));
     var price_per_square_inch = parseFloat((parseFloat(price_input.value) / area).toFixed(2));
+    if (Number.isNaN(area) || Number.isNaN(price_per_square_inch)) {
+        alert("There was a calculation error. Please make sure that you input the numbers correctly then try again.");
+        return;
+    }
+    else if (!Number.isFinite(price_per_square_inch)) {
+        alert("There was a calculation error. Please make sure that any of the input numbers aren't zero's.");
+        return;
+    }
     calc_result.value = "price per isq: ".concat(price_per_square_inch, " | area: ").concat(area);
+}
+function test() {
+    console.log("greg ".concat(1 + 1));
 }
--- src/script.ts
+++ src/script.ts
@@ -4,8 +4,20 @@ const calc_result: HTMLInputElement = document.querySelector("#calc_result");
 const pi: number = 3.14159265359;
 
 function calculate(): void {
-  var area = parseFloat((pi*(parseFloat(diameter_input.value)/2)**2).toFixed(2));
-  var price_per_square_inch = parseFloat((parseFloat(price_input.value)/area).toFixed(2));
+	var area: number = parseFloat((pi*(parseFloat(diameter_input.value)/2)**2).toFixed(2));
+	var price_per_square_inch: number = parseFloat((parseFloat(price_input.value)/area).toFixed(2));
 
-  calc_result.value = `price per isq: ${price_per_square_inch} | area: ${area}`;
+	if (Number.isNaN(area) || Number.isNaN(price_per_square_inch)) {
+		alert("There was a calculation error. Please make sure that you input the numbers correctly then try again.");
+		return;
+	} else if (!Number.isFinite(price_per_square_inch)) {
+		alert("There was a calculation error. Please make sure that any of the input numbers aren't zero's.");
+		return;
+	}
+
+	calc_result.value = `price per isq: ${price_per_square_inch} | area: ${area}`;
+}
+
+function test(): void {
+	console.log(`greg ${1+1}`);
 }
